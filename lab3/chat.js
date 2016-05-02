b1cf33110acb412c0a291ced99a683e0b2411b2b
fridahau <fridahau@kth.se>
2016-05-02 15:28:27 +0200

    börjat med labb 3

---
+++ lab3/chat.js
@@ -0,0 +1,55 @@
+(function() {
+
+  var output = document.querySelector('#output'),
+    input = document.querySelector('#input'),
+    button = document.querySelector('#button'),
+    avatar = document.querySelector('#avatar'),
+    presence = document.querySelector('#presence');
+  var channel = 'mchat';
+
+  // Assign a random avatar in random color
+  if (avatar!=null){
+      avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+  }
+  
+
+  var p = PUBNUB.init({
+    subscribe_key: 'sub-c-9f3447de-105e-11e6-bb6c-02ee2ddab7fe',
+    publish_key: 'pub-c-5d54bf3a-2c7f-4505-9007-e6b0f27c9790'
+  });
+// pub-c-ce04f67b-0f26-43ce-8be2-192e9821d1a3
+// sub-c-182105ac-0001-11e5-8fd4-0619f8945a4f
+  
+
+  p.subscribe({
+    channel: channel,
+    callback: function(m) {
+      output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' + m.text.replace(/[<>]/ig, '') + '</span></p>' + output.innerHTML;
+    },
+    presence: function(m) {
+      if (m.occupancy > 1) {
+        presence.textContent = m.occupancy + ' people online';
+      } else {
+        presence.textContent = 'Nobody else is online';
+      }
+    }
+  });
+
+  p.bind('keyup', input, function(e) {
+    (e.keyCode || e.charCode) === 13 && publish()
+  });
+
+  p.bind('click', button, publish);
+
+  function publish() {
+    p.publish({
+      channel: channel,
+      message: {
+        avatar: avatar.className,
+        text: input.value
+      },
+      x: (input.value = '')
+    });
+  }
+
+})();
